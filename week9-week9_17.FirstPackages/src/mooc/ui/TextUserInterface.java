c7ee4e254481203b9b0a12118d1259e0bb1ed298
Dawid Bińkuś <dbinkus96@gmail.com>
2017-08-18 00:05:58 +0200

    week 09 - exercise 017

---
+++ week9-week9_17.FirstPackages/src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,13 @@
+package mooc.ui;
+
+/**
+ * Created by Dawid on 04.08.2017.
+ */
+public class TextUserInterface implements UserInterface{
+
+
+    @Override
+    public void update() {
+        System.out.println("Updating the user interface");
+    }
+}
---
+++ week9-week9_17.FirstPackages/src/mooc/ui/UserInterface.java
@@ -0,0 +1,10 @@
+package mooc.ui;
+
+/**
+ * Created by Dawid on 04.08.2017.
+ */
+public interface UserInterface {
+
+    void update();
+
+}
