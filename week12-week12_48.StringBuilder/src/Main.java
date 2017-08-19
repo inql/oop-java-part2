83e34bfd50662181e2bd3b1946b809aa1b183bef
Dawid Bińkuś <dbinkus96@gmail.com>
2017-08-20 00:30:16 +0200

    week 12 - exercise 048

---
+++ week12-week12_48.StringBuilder/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(build(t));
+    }
+
+    public static String build(int[] t) {
+        StringBuilder builder = new StringBuilder(35);
+        builder.append("{\n ");
+        for(int i =0; i<t.length; i++)
+        {
+            builder.append(t[i]);
+            if (i==t.length-1)
+            {
+                builder.append('\n');
+                continue;
+            }
+
+            builder.append(", ");
+            if(t[i]%4==0)
+                builder.append("\n ");
+
+        }
+        builder.append('}');
+        return builder.toString();
+    }
+}
